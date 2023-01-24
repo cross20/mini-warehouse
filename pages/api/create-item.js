fc0b2bf0cae79b809d4e9521b6b6f1b7bbf7ae40
Chad Ross <rosschad98@icloud.com>
2023-01-23 16:58:55 -0800

    Replace condition ID with condition

--- pages/api/create-item.js
+++ pages/api/create-item.js
@@ -1,21 +1,28 @@
 export default function handler(req, res) {
-    const { name, description, quantity, categoryId, locationId, conditionId, brandId } = req.body
+    const {
+        name, 
+        description, 
+        quantity, 
+        condition, 
+        categoryId, 
+        locationId, 
+        brandId
+    } = req.body
 
     const item = {
         name: name,
         description: description,
         quantity: quantity,
+        condition: condition,
         categoryId: categoryId,
         locationId: locationId,
-        conditionId: conditionId,
-        brandId: brandId,
-        createdAt: Date.now(),
+        brandId: brandId
     }
 
-    console.log('item', item);
+    // TODO: validate data.
 
     const max = 1000;
-    const id = Math.floor(Math.random() * max)
+    const id = Math.floor(Math.random() * max) // TODO: generate real ID.
 
     res.status(200).json({ id: id })
 }
